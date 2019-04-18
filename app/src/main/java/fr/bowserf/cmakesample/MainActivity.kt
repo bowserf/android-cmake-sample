c2b5efc78b39835a39b322d400f7889c65568551
frederic.torcheux <frederic.torcheux@djit.fr>
2019-04-18 14:54:50 +0200

    [Project] Initial commit

---
+++ app/src/main/java/fr/bowserf/cmakesample/MainActivity.kt
@@ -0,0 +1,29 @@
+package fr.bowserf.cmakesample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sample_text.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
