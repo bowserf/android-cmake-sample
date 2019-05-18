f87d3a95d71fc1cd0d2083e9c8ded16ef91789ec
frederic.torcheux <frederic.torcheux@djit.fr>
2019-05-18 21:04:13 +0200

    [UI] Improve the layout to display selected operator

--- app/src/main/java/fr/bowserf/cmakesample/MainActivity.kt
+++ app/src/main/java/fr/bowserf/cmakesample/MainActivity.kt
@@ -18,25 +18,30 @@ class MainActivity : AppCompatActivity() {
         val editTextVal1 = findViewById<EditText>(R.id.edit_value1)
         val editTextVal2 = findViewById<EditText>(R.id.edit_value2)
 
+        val operator = findViewById<TextView>(R.id.tv_multiply_symbol)
+
         findViewById<Button>(R.id.btn_multiply).setOnClickListener {
             val value1 = editTextVal1.text.toString().toLong()
             val value2 = editTextVal2.text.toString().toLong()
             val result = calculator.multiply(value1, value2)
-            tvResultComputation.text = resources.getString(R.string.multiplication_result, result)
+            tvResultComputation.text = result.toString()
+            operator.text = "x"
         }
 
         findViewById<Button>(R.id.btn_addition).setOnClickListener {
             val value1 = editTextVal1.text.toString().toLong()
             val value2 = editTextVal2.text.toString().toLong()
             val result = calculator.add(value1, value2)
-            tvResultComputation.text = resources.getString(R.string.addition_result, result)
+            tvResultComputation.text = result.toString()
+            operator.text = "+"
         }
 
         findViewById<Button>(R.id.btn_subtraction).setOnClickListener {
             val value1 = editTextVal1.text.toString().toLong()
             val value2 = editTextVal2.text.toString().toLong()
             val result = calculator.minus(value1, value2)
-            tvResultComputation.text = resources.getString(R.string.subtraction_result, result)
+            tvResultComputation.text = result.toString()
+            operator.text = "-"
         }
     }
 
